1815c2d9f41c27623536874eee95ae3526f725d2
dovile <dovile.tarutyte@hostinger.com>
2017-09-26 13:17:59 +0300

    Edit css

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -32,6 +32,8 @@
     <meta name="theme-color" content="#ffffff">
 
     <link href="https://fonts.googleapis.com/css?family=PT+Mono|Roboto:100,300,400,500,700&amp;subset=cyrillic,cyrillic-ext" rel="stylesheet">
+
+
     <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:100,200,300,400,500,600,700,900|Work+Sans:100,200,300,400,500,600,700,800,900&amp;subset=latin-ext" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
--- resources/views/partials/landing/team.blade.php
+++ resources/views/partials/landing/team.blade.php
@@ -8,190 +8,203 @@
         </div>
 
         <div id="js-team-members" class="row team-members">
-            <div class="col-xs-12 col-sm-3 wow fadeIn">
-                <div class="team-member">
-                    <div class="team-member-image-holder ">
 
-                        <img class="team-member-image" src="{{ asset('team/andrius-putna.png') }}" alt="Andrius Putna">
+            <div class="row">
 
-                    </div>
-                    <div class="team-member-data">
-                        <h3 class="title">Andrius <b>Putna</b></h3>
-                        <p>@lang('team.position_ceo')</p>
-                        <div class="handles">
-                            <a href="https://www.linkedin.com/in/andriusputna/" rel="nofollow" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                <div class="col-xs-12 col-sm-3 wow fadeIn">
+                    <div class="team-member">
+                        <div class="team-member-image-holder ">
+
+                            <img class="team-member-image" src="{{ asset('team/andrius-putna.png') }}" alt="Andrius Putna">
+
                         </div>
+                        <div class="team-member-data">
+                            <h3 class="title">Andrius <b>Putna</b></h3>
+                            <p>@lang('team.position_ceo')</p>
+                            <div class="handles">
+                                <a href="https://www.linkedin.com/in/andriusputna/" rel="nofollow" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                            </div>
 
-                        <div class="team-member-desc">
-                            <p>@lang('team.description_ceo')</p>
+                            <div class="team-member-desc">
+                                <p>@lang('team.description_ceo')</p>
+                            </div>
+
                         </div>
-
                     </div>
                 </div>
+
+                <div class="col-xs-12 col-sm-3 wow fadeIn">
+                    <div class="team-member">
+                        <div class="team-member-image-holder">
+                            <img class="team-member-image" src="{{ asset('team/danielius-stasiulis.png') }}" alt="Danielius Stasiulis">
+
+                        </div>
+                        <div class="team-member-data">
+                            <h3 class="title">Danielius <b>Stasiulis</b></h3>
+                            <p>@lang('team.position_cbo')</p>
+                            <div class="handles">
+                                <a href="https://www.linkedin.com/in/danius/" rel="nofollow" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                            </div>
+
+                            <div class="team-member-desc">
+                                <p>@lang('team.description_cbo')</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-3 wow fadeIn">
+                    <div class="team-member">
+                        <div class="team-member-image-holder">
+                            <img class="team-member-image" src="{{ asset('team/martynas-andriuskevicius.png') }}" alt="Martynas Andriuškevičius">
+
+                        </div>
+                        <div class="team-member-data">
+                            <h3 class="title">Martynas <b>Andriuškevičius</b></h3>
+                            <p>@lang('team.position_cto')</p>
+                            <div class="handles">
+                                <a href="https://www.linkedin.com/in/martynas-andriuskevicius/" rel="nofollow" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                            </div>
+
+                            <div class="team-member-desc">
+                                <p>@lang('team.description_cto')</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
-            <div class="col-xs-12 col-sm-3 wow fadeIn">
-                <div class="team-member">
-                    <div class="team-member-image-holder">
-                        <img class="team-member-image" src="{{ asset('team/danielius-stasiulis.png') }}" alt="Danielius Stasiulis">
+            <div class="row">
 
-                    </div>
-                    <div class="team-member-data">
-                        <h3 class="title">Danielius <b>Stasiulis</b></h3>
-                        <p>@lang('team.position_cbo')</p>
-                        <div class="handles">
-                            <a href="https://www.linkedin.com/in/danius/" rel="nofollow" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                <div class="col-xs-12 col-sm-3 wow fadeIn">
+                    <div class="team-member">
+                        <div class="team-member-image-holder">
+                            <img class="team-member-image" src="{{ asset('team/ervinas-rimdeika.png') }}" alt="Ervinas Rimdeika">
+
                         </div>
+                        <div class="team-member-data">
+                            <h3 class="title">Ervinas <b>Rimdeika</b></h3>
+                            <p>@lang('team.position_legal')</p>
+                            <div class="handles">
+                                <a href="https://www.linkedin.com/in/ervinas-rimdeika-0528329/" rel="nofollow" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                            </div>
 
-                        <div class="team-member-desc">
-                            <p>@lang('team.description_cbo')</p>
+                            <div class="team-member-desc">
+                                <p>@lang('team.description_legal')</p>
+                            </div>
+
                         </div>
-
                     </div>
                 </div>
+
+                <div class="col-xs-12 col-sm-3 wow fadeIn">
+                    <div class="team-member">
+                        <div class="team-member-image-holder">
+                            <img class="team-member-image" src="{{ asset('team/darius-rugevicius.png') }}" alt="Darius Rugevičius">
+                        </div>
+                        <div class="team-member-data">
+                            <h3 class="title">Darius <b>Rugevičius</b></h3>
+                            <p>@lang('team.position_capital_investment')</p>
+                            <div class="handles">
+                                <a href="https://www.linkedin.com/in/dariusrugevicius/" rel="nofollow" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                            </div>
+
+                            <div class="team-member-desc">
+                                <p>@lang('team.description_capital_investment')</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-3 wow fadeIn">
+                    <div class="team-member">
+                        <div class="team-member-image-holder">
+                            <img class="team-member-image" src="{{ asset('team/rio-asatiani.png') }}" alt="R.A.">
+
+                        </div>
+                        <div class="team-member-data">
+                            <h3 class="title">Rio <b>Asatiani</b></h3>
+                            <p>@lang('team.position_cmo')</p>
+                            <div class="handles">
+                                <i class="fa " aria-hidden="true"></i>
+                            </div>
+
+                            <div class="team-member-desc">
+                                <p>@lang('team.description_cmo')</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
-            <div class="col-xs-12 col-sm-3 wow fadeIn">
-                <div class="team-member">
-                    <div class="team-member-image-holder">
-                        <img class="team-member-image" src="{{ asset('team/martynas-andriuskevicius.png') }}" alt="Martynas Andriuškevičius">
+            <div class="row">
 
-                    </div>
-                    <div class="team-member-data">
-                        <h3 class="title">Martynas <b>Andriuškevičius</b></h3>
-                        <p>@lang('team.position_cto')</p>
-                        <div class="handles">
-                            <a href="https://www.linkedin.com/in/martynas-andriuskevicius/" rel="nofollow" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                <div class="col-xs-12 col-sm-3 wow fadeIn">
+                    <div class="team-member">
+                        <div class="team-member-image-holder">
+                            <img class="team-member-image" src="{{ asset('team/julija-sabaliauskaite.png') }}" alt="Julija Sabaliauskaitė">
+
                         </div>
+                        <div class="team-member-data">
+                            <h3 class="title">Julija <b>Sabaliauskaitė</b></h3>
+                            <p>@lang('team.position_business_assistant')</p>
+                            <div class="handles">
+                            </div>
 
-                        <div class="team-member-desc">
-                            <p>@lang('team.description_cto')</p>
+                            <div class="team-member-desc">
+                                <p>@lang('team.description_business_assistant')</p>
+                            </div>
+
                         </div>
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-3 wow fadeIn">
-                <div class="team-member">
-                    <div class="team-member-image-holder">
-                        <img class="team-member-image" src="{{ asset('team/ervinas-rimdeika.png') }}" alt="Ervinas Rimdeika">
-
-                    </div>
-                    <div class="team-member-data">
-                        <h3 class="title">Ervinas <b>Rimdeika</b></h3>
-                        <p>@lang('team.position_legal')</p>
-                        <div class="handles">
-                            <a href="https://www.linkedin.com/in/ervinas-rimdeika-0528329/" rel="nofollow" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
-                        </div>
-
-                        <div class="team-member-desc">
-                            <p>@lang('team.description_legal')</p>
-                        </div>
-
                     </div>
                 </div>
-            </div>
 
-            <div class="col-xs-12 col-sm-3 wow fadeIn">
-                <div class="team-member">
-                    <div class="team-member-image-holder">
-                        <img class="team-member-image" src="{{ asset('team/darius-rugevicius.png') }}" alt="Darius Rugevičius">
-                    </div>
-                    <div class="team-member-data">
-                        <h3 class="title">Darius <b>Rugevičius</b></h3>
-                        <p>@lang('team.position_capital_investment')</p>
-                        <div class="handles">
-                            <a href="https://www.linkedin.com/in/dariusrugevicius/" rel="nofollow" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                <div class="col-xs-12 col-sm-3 wow fadeIn">
+                    <div class="team-member">
+                        <div class="team-member-image-holder">
+                            <img class="team-member-image" src="{{ asset('team/sarune-saulyte.png') }}" alt="Šarūnė Šaulytė">
+
                         </div>
+                        <div class="team-member-data">
+                            <h3 class="title">Šarūnė <b>Šaulytė</b></h3>
+                            <p>@lang('team.position_marketing_assistant')</p>
+                            <div class="handles">
+                            </div>
 
-                        <div class="team-member-desc">
-                            <p>@lang('team.description_capital_investment')</p>
+                            <div class="team-member-desc">
+                                <p>@lang('team.description_marketing_assistant')</p>
+                            </div>
+
                         </div>
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-3 wow fadeIn">
-                <div class="team-member">
-                    <div class="team-member-image-holder">
-                        <img class="team-member-image" src="{{ asset('team/rio-asatiani.png') }}" alt="R.A.">
-
-                    </div>
-                    <div class="team-member-data">
-                        <h3 class="title">Rio <b>Asatiani</b></h3>
-                        <p>@lang('team.position_cmo')</p>
-                        <div class="handles">
-                            <i class="fa " aria-hidden="true"></i>
-                        </div>
-
-                        <div class="team-member-desc">
-                            <p>@lang('team.description_cmo')</p>
-                        </div>
-
                     </div>
                 </div>
-            </div>
 
-            <div class="col-xs-12 col-sm-3 wow fadeIn">
-                <div class="team-member">
-                    <div class="team-member-image-holder">
-                        <img class="team-member-image" src="{{ asset('team/julija-sabaliauskaite.png') }}" alt="Julija Sabaliauskaitė">
+                <div class="col-xs-12 col-sm-3 wow fadeIn">
+                    <div class="team-member">
+                        <div class="team-member-image-holder">
+                            <img class="team-member-image" src="{{ asset('team/dovile-tarutyte.png') }}" alt="Dovilė Tarutytė">
 
-                    </div>
-                    <div class="team-member-data">
-                        <h3 class="title">Julija <b>Sabaliauskaitė</b></h3>
-                        <p>@lang('team.position_business_assistant')</p>
-                        <div class="handles">
                         </div>
+                        <div class="team-member-data">
+                            <h3 class="title">Dovilė <b>Tarutytė</b></h3>
+                            <p>@lang('team.position_designer')</p>
+                            <div class="handles">
+                            </div>
 
-                        <div class="team-member-desc">
-                            <p>@lang('team.description_business_assistant')</p>
+                            <div class="team-member-desc">
+                                <p>@lang('team.description_designer')</p>
+                            </div>
+
                         </div>
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-3 wow fadeIn">
-                <div class="team-member">
-                    <div class="team-member-image-holder">
-                        <img class="team-member-image" src="{{ asset('team/sarune-saulyte.png') }}" alt="Šarūnė Šaulytė">
-
-                    </div>
-                    <div class="team-member-data">
-                        <h3 class="title">Šarūnė <b>Šaulytė</b></h3>
-                        <p>@lang('team.position_marketing_assistant')</p>
-                        <div class="handles">
-                        </div>
-
-                        <div class="team-member-desc">
-                            <p>@lang('team.description_marketing_assistant')</p>
-                        </div>
-
                     </div>
                 </div>
-            </div>
 
-            <div class="col-xs-12 col-sm-3 wow fadeIn">
-                <div class="team-member">
-                    <div class="team-member-image-holder">
-                        <img class="team-member-image" src="{{ asset('team/dovile-tarutyte.png') }}" alt="Dovilė Tarutytė">
-
-                    </div>
-                    <div class="team-member-data">
-                        <h3 class="title">Dovilė <b>Tarutytė</b></h3>
-                        <p>@lang('team.position_designer')</p>
-                        <div class="handles">
-                        </div>
-
-                        <div class="team-member-desc">
-                            <p>@lang('team.description_designer')</p>
-                        </div>
-
-                    </div>
-                </div>
             </div>
 
 
