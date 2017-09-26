88e1f136eb4e506a7718001b50dffa914f23a53c
dovile <dovile.tarutyte@hostinger.com>
2017-09-26 17:11:16 +0300

    Fix some css

--- resources/views/partials/ico-progress.blade.php
+++ resources/views/partials/ico-progress.blade.php
@@ -20,17 +20,16 @@
 
                 <div class="progress-bar-wrapper">
                     <div style="position: relative; margin-bottom: 2em; background: rgba(177, 177, 177, 0.52); box-shadow: inset 0 1px 0 0 rgba(249, 249, 249, 0.11);">
-                        <!-- soft cap marker -->
-                        <div style="z-index: 20; position: absolute; top: 0; bottom:0; width: 1px; height: 30px; left: {{ $softCapEth / $hardCapEth * 100 }}%; background: #fff;"> <h3 class="soft-cap-text">Soft Cap: {{ number_format($softCapEth, 0, ".", "") }} ETH</h3></div>
-
-
-                        <!-- hard cap marker -->
-                        <div style="z-index: 20; position: absolute; top: 0; bottom:0; width: 1px; height: 30px; right: 0; background: #fff;"> <h3 class="hard-cap-text">Hard Cap: {{ number_format($hardCapEth, 0, ".", "") }} ETH</h3></div>
-
-
                         <!-- progress bar -->
                         <div style="width: {{ $progress }}%; height: 30px; background-image: linear-gradient(-180deg, #F93800 4%, #c02b3f 99%); box-shadow: inset 0 2px 0 0 #ff6868; position: relative;"></div>
                     </div>
+                    <!-- soft cap marker -->
+                    <div class="soft-cap-marker" style="z-index: 20; position: absolute; top: 0; bottom:0; width: 1px; height: 30px; left: {{ $softCapEth / $hardCapEth * 100 }}%; background: #fff;"> <h3 class="soft-cap-text">Soft Cap: {{ number_format($softCapEth, 0, ".", "") }} ETH</h3></div>
+
+
+                    <!-- hard cap marker -->
+                    <div class="hard-cap-marker"  style="z-index: 20; position: absolute; top: 0; bottom:0; width: 1px; height: 30px; right: 0; background: #fff;"> <h3 class="hard-cap-text">Hard Cap: {{ number_format($hardCapEth, 0, ".", "") }} ETH</h3></div>
+
                 </div>
             </div>
         </div>
